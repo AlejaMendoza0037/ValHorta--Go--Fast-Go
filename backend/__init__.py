2c884264fb3bc0f6a0c4203f46b71286452b387f
Aleja Mendoza <alejamendoza0@gmail.com>
2024-03-29 12:54:41 -0500

    version 1.1

--- backend/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# Importa las rutas después de inicializar la aplicación para evitar problemas de importación circular
-from app import routes
--- backend/models.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-# Función para conectar a la base de datos SQLite
-def connect_db():
-    return sqlite3.connect('db/usuarios.db')
